933f2c3cece98f5bca5a0d034542a9dcba136c20
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-07-03 02:22:54 +0200

    converted JASPAR motifs to PWMs in cluster-buster formats

---
+++ submissions/leaderboard/004_jaspar-motifs_freq.tf
@@ -0,0 +1,124 @@
+AC  MA1999.2
+XX
+ID  Prdm5
+XX
+DE  MA1999.2 Prdm5 ; From JASPAR
+P0           A         C         G         T
+1      0.04865   0.08108   0.78378   0.08649
+2      0.07973   0.11622   0.06757   0.73649
+3      0.02432   0.05811   0.05000   0.86757
+4      0.02703   0.91486   0.01757   0.04054
+5      0.05270   0.04189   0.02703   0.87838
+6      0.02162   0.91486   0.04054   0.02297
+7      0.02973   0.92568   0.02432   0.02027
+8      0.77703   0.10946   0.05405   0.05946
+9      0.02432   0.07973   0.17297   0.72297
+10     0.05811   0.70000   0.11622   0.12568
+11     0.11892   0.19595   0.09054   0.59459
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: MA1999.2
+CC  AC: MA1999.2
+CC  id: MA1999.2
+CC  name: Prdm5
+CC  version: 
+CC  name: Prdm5
+CC  description: MA1999.2 Prdm5 ; From JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+XX
+//
+AC  UN0812.1
+XX
+ID  ZNF366
+XX
+DE  UN0812.1 ZNF366 ; From JASPAR
+P0           A         C         G         T
+1      0.08819   0.31184   0.10635   0.49363
+2      0.08075   0.53666   0.11368   0.26891
+3      0.00244   0.99256   0.00467   0.00032
+4      0.00574   0.00436   0.03570   0.95421
+5      0.00765   0.98863   0.00308   0.00064
+6      0.00701   0.98183   0.00999   0.00117
+7      0.00977   0.00414   0.03634   0.94975
+8      0.00319   0.99564   0.00053   0.00064
+9      0.09499   0.53485   0.09520   0.27497
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 2
+CC  accession: UN0812.1
+CC  AC: UN0812.1
+CC  id: UN0812.1
+CC  name: ZNF366
+CC  version: 
+CC  name: ZNF366
+CC  description: UN0812.1 ZNF366 ; From JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 2
+XX
+//
+AC  MA1726.2
+XX
+ID  ZNF331
+XX
+DE  MA1726.2 ZNF331 ; From JASPAR
+P0           A         C         G         T
+1      0.13183   0.05291   0.07199   0.74328
+2      0.01041   0.01214   0.96704   0.01041
+3      0.01474   0.96618   0.00781   0.01127
+4      0.74935   0.02428   0.03816   0.18820
+5      0.01388   0.00867   0.97225   0.00520
+6      0.95230   0.01995   0.01648   0.01127
+7      0.05551   0.00954   0.93235   0.00260
+8      0.01561   0.95663   0.01301   0.01474
+9      0.14310   0.73287   0.03643   0.08760
+10     0.14137   0.57242   0.12663   0.15958
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 3
+CC  accession: MA1726.2
+CC  AC: MA1726.2
+CC  id: MA1726.2
+CC  name: ZNF331
+CC  version: 
+CC  name: ZNF331
+CC  description: MA1726.2 ZNF331 ; From JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 3
+XX
+//
+AC  MA0062.4
+XX
+ID  GABPA
+XX
+DE  MA0062.4 GABPA ; From JASPAR
+P0           A         C         G         T
+1      0.11666   0.63223   0.10674   0.14437
+2      0.79812   0.06091   0.07442   0.06656
+3      0.01419   0.85010   0.03313   0.10258
+4      0.05933   0.02204   0.01474   0.90389
+5      0.01316   0.01878   0.02494   0.94311
+6      0.02135   0.93729   0.01756   0.02379
+7      0.01517   0.96377   0.00794   0.01312
+8      0.02630   0.02355   0.06966   0.88050
+9      0.03389   0.09664   0.79293   0.07654
+10     0.08161   0.18336   0.11790   0.61713
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 4
+CC  accession: MA0062.4
+CC  AC: MA0062.4
+CC  id: MA0062.4
+CC  name: GABPA
+CC  version: 
+CC  name: GABPA
+CC  description: MA0062.4 GABPA ; From JASPAR
+CC  transfac_consensus: 
+CC  matrix.nb: 4
+XX
+//
