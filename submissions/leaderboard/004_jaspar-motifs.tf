3db11411bafc4d070a6f0c6de3eccf880db1f32c
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-07-03 02:09:01 +0200

    added submissions

---
+++ submissions/leaderboard/004_jaspar-motifs.tf
@@ -0,0 +1,100 @@
+AC MA1999.2
+XX
+ID Prdm5
+XX
+DE MA1999.2 Prdm5 ; From JASPAR
+PO	A	C	G	T
+01	36.0	60.0	580.0	64.0
+02	59.0	86.0	50.0	545.0
+03	18.0	43.0	37.0	642.0
+04	20.0	677.0	13.0	30.0
+05	39.0	31.0	20.0	650.0
+06	16.0	677.0	30.0	17.0
+07	22.0	685.0	18.0	15.0
+08	575.0	81.0	40.0	44.0
+09	18.0	59.0	128.0	535.0
+10	43.0	518.0	86.0	93.0
+11	88.0	145.0	67.0	440.0
+XX
+CC tax_group:vertebrates
+CC tf_family:More than 3 adjacent zinc fingers
+CC tf_class:C2H2 zinc finger factors
+CC pubmed_ids:22589746
+CC uniprot_ids:Q9CXE0
+CC data_type:
+XX
+//
+AC UN0812.1
+XX
+ID ZNF366
+XX
+DE UN0812.1 ZNF366 ; From JASPAR
+PO	A	C	G	T
+01	830.0	2935.0	1001.0	4646.0
+02	760.0	5051.0	1070.0	2531.0
+03	23.0	9342.0	44.0	3.0
+04	54.0	41.0	336.0	8981.0
+05	72.0	9305.0	29.0	6.0
+06	66.0	9241.0	94.0	11.0
+07	92.0	39.0	342.0	8939.0
+08	30.0	9371.0	5.0	6.0
+09	894.0	5034.0	896.0	2588.0
+XX
+CC tax_group:vertebrates
+CC tf_family:More than 3 adjacent zinc fingers
+CC tf_class:C2H2 zinc finger factors
+CC pubmed_ids:
+CC uniprot_ids:Q8N895
+CC data_type:ChIP-seq
+XX
+//
+AC MA1726.2
+XX
+ID ZNF331
+XX
+DE MA1726.2 ZNF331 ; From JASPAR
+PO	A	C	G	T
+01	152.0	61.0	83.0	857.0
+02	12.0	14.0	1115.0	12.0
+03	17.0	1114.0	9.0	13.0
+04	864.0	28.0	44.0	217.0
+05	16.0	10.0	1121.0	6.0
+06	1098.0	23.0	19.0	13.0
+07	64.0	11.0	1075.0	3.0
+08	18.0	1103.0	15.0	17.0
+09	165.0	845.0	42.0	101.0
+10	163.0	660.0	146.0	184.0
+XX
+CC tax_group:vertebrates
+CC tf_family:More than 3 adjacent zinc fingers
+CC tf_class:C2H2 zinc finger factors
+CC pubmed_ids:27852650
+CC uniprot_ids:Q9NQX6
+CC data_type:ChIP-seq
+XX
+//
+AC MA0062.4
+XX
+ID GABPA
+XX
+DE MA0062.4 GABPA ; From JASPAR
+PO      A       C       G       T
+01      7639.0  41398.0 6989.0  9453.0
+02      52260.0 3988.0  4873.0  4358.0
+03      929.0   55664.0 2169.0  6717.0
+04      3885.0  1443.0  965.0   59186.0
+05      862.0   1230.0  1633.0  61754.0
+06      1398.0  61373.0 1150.0  1558.0
+07      993.0   63107.0 520.0   859.0
+08      1722.0  1542.0  4561.0  57654.0
+09      2219.0  6328.0  51920.0 5012.0
+10      5344.0  12006.0 7720.0  40409.0
+XX
+CC tax_group:vertebrates
+CC tf_family:Ets-related
+CC tf_class:Tryptophan cluster factors
+CC pubmed_ids:9461436
+CC uniprot_ids:Q06546
+CC data_type:ChIP-seq
+XX
+//
