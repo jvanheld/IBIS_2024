2b3cab0aa69350456d53fc3b5c44a82c1b401f62
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-07-18 15:26:04 +0200

    added detection of over-rperesented oligonucleotides in train set with respect to the test sequences

--- makefiles/03_oligo-analysis.mk
+++ makefiles/03_oligo-analysis.mk
@@ -5,7 +5,7 @@
 include makefiles/00_parameters.mk
 MAKEFILE=makefiles/03_oligo-analysis.mk
 
-param:
+param: param_00
 	@echo "dataset parameters"
 	@echo "	BOARD		${BOARD}"
 	@echo "	DATA_TUPE	${DATA_TYPE}"
@@ -14,22 +14,29 @@ param:
 	@echo "	MAXOL		${MAXOL}"
 	@echo "	BG_DIR		${BG_DIR}"
 	@echo "	BG_FILE		${BG_FILE}"
+	@echo "	OLIGO_DIR	${OLIGO_DIR}"
+	@echo "	OLIGO_SCRIPT	${OLIGO_SCRIPT}"
 	@echo "	OLIGO_CMD	${OLIGO_CMD}"
+	@echo "	OLIGOS		${OLIGOS}"
+	@echo "	ASSEMBLY_CMD	${ASSEMBLY_CMD}"
+	@echo "	ASSEMBLY	${ASSEMBLY}"
+	@echo "	MATRICES_CMD	${MATRICES_CMD}"
+	@echo "	MATRICES	${MATRICES}"
 	@echo 
 
-targets:
+targets: targets_00
 	@echo "Targets"
 	@echo "	bg_freq		compute background frequencies"
+	@echo "	oligos		detect over-represented oligos"
 
 
 V=1
 OL=6
+BG_OL=${OL}
 MINOL=6
 MAXOL=7
 SEQ_SET=YWE_B_AffSeq_C12
-TRAIN_PATH=data/${BOARD}/train/${DATA_TYPE}/${TF}/${SEQSET}_${TF}.C2
-TRAIN_COORD=${TRAIN_PATH}.peaks
-TRAIN_SEQ=${TRAIN_PATH}.fasta
+QUICK=quick
 
 ################################################################
 ## Compute background frequencies for oligonucleotide
@@ -44,7 +51,8 @@ bg_freq_one_size:
 	@echo
 	@echo "Computing background frequencies for ${BG_OL}nt"
 	@mkdir -p ${BG_DIR}
-	time oligo-analysis -quick -v ${V} \
+	time oligo-analysis ${QUICK} \
+		-v ${V} \
 		-i ${TEST_SEQ} \
 		-l ${BG_OL} \
 		-noov \
@@ -59,16 +67,81 @@ bg_freq_one_size:
 
 ################################################################
 ## Detect over-represented oligonucleotides
-OLIGO_CMD=oligo-analysis -v ${V} -i ${TRAIN_SEQ} \
-	–noov -2str -grouprc -seqtype dna \
-	-format fasta -expfreq ${BG_FILE} \
-	-pseudo 0.05 \
-	-return occ,freq,proba,rank -sort \
-	-lth occ_sig 10 -uth rank 50 -lth sig 1 \
-	-o ${OLIGOS}
-oligos:
-	@echo
-	@echo "Detecting over-represented oligonucleotides"
+OLIGO_DIR=${RESULT_DIR}/oligos
+OLIGOS=
+OLIGO_PATH=${OLIGO_DIR}/${OL}nt-noov-2str_${BOARD}_${TF}_${PEAKSET}
+OLIGOS=${OLIGO_PATH}.tsv
+MERGED_OLIGO_PATH=${OLIGO_DIR}/${MINOL}-${MAXOL}nt-noov-2str_${BOARD}_${TF}_${PEAKSET}
+MERGED_OLIGOS=${MERGED_OLIGO_PATH}.tsv
+ASSEMBLY=${MERGED_OLIGO_PATH}_asmb.txt
+MATRICES=${MERGED_OLIGO_PATH}_pssm
 
+OLIGO_CMD=oligo-analysis -v ${V} \
+	-i ${PEAK_SEQ} \
+	-l ${OL} \
+	–noov \
+	-2str \
+	-grouprc \
+	-seqtype dna \
+	-format fasta \
+	-expfreq ${BG_FILE} \
+	-pseudo 0.05 \
+	-return occ,freq,proba,rank \
+	-sort \
+	-lth occ_sig 10 \
+	-uth rank 50 \
+	-lth occ_sig 1 \
+	-o ${OLIGOS}
+ASSEMBLY_CMD=pattern-assembly -v ${V} \
+	-subst 1 \
+	-toppat 50 \
+	-2str \
+	-max_asmb_nb 20 \
+	-i ${OLIGOS} \
+	-o ${ASSEMBLY}
+MATRIX_CMD=matrix-from-patterns \
+	-v ${V} \
+	-logo  \
+	-seq ${PEAK_SEQ} \
+	-format fasta \
+	-asmb ${ASSEMBLY} \
+	-min_weight 5 \
+	-flanks 2 \
+	-max_asmb_nb 20 \
+	-cluster sig \
+	-uth Pval 0.00025 \
+	-bginput \
+	-markov 0 \
+	-o ${MATRICES}
+
+OLIGO_SCRIPT=${OLIGO_DIR}/oligo_script.sh
+oligos:
+	@echo "Detecting over-represented oligonucleotides"
+	@echo "	OLIGO_DIR	${OLIGO_DIR}"
+	@mkdir -p ${OLIGO_DIR}
+	@echo
+	@echo "Writing oligo-analysis script	${OLIGO_SCRIPT}"
+	@echo ${SBATCH_HEADER} > ${OLIGO_SCRIPT}
+	@for ol in `seq ${MINOL} ${MAXOL}`; do \
+		${MAKE} oligos_one_len OL=$${ol} ; \
+	done
+	@echo >> ${OLIGO_SCRIPT}
+	@echo ${ASSEMBLY_CMD} >> ${OLIGO_SCRIPT}
+	@echo >> ${OLIGO_SCRIPT}
+	@echo ${MATRIX_CMD} >> ${OLIGO_SCRIPT}
+	@echo
+	@echo "	OLIGO_SCRIPT	${OLIGO_SCRIPT}"
+	@echo "Running peak-motifs"
+#	@${SBATCH} ${OLIGO_SCRIPT}
+	@echo
+	@echo "	OLIGO_DIR	${OLIGO_DIR}"
+	@echo "	OLIGOS		${OLIGOS}"
+	@echo "	ASSEMBLY	${ASSEMBLY}"
+	@echo "	MATRICES	${MATRICES}"
+
+oligos_one_len:
+	@echo "oligo-analysis	${OL}nt"
+	@echo >> ${OLIGO_SCRIPT}
+	@echo ${OLIGO_CMD} >> ${OLIGO_SCRIPT}
 
 
