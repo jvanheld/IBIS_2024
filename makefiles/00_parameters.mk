5e6f0c5c955edc4fc91932e99b9f727860bb02b4
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-07-24 18:21:26 +0200

    adapted parameters to read motif databases from the Docker container

--- makefiles/00_parameters.mk
+++ makefiles/00_parameters.mk
@@ -16,9 +16,9 @@ RSAT_CMD=rsat
 MOTIFDB_DIR=/shared/projects/rsat_organism/motif_databases
 
 ## Local configuration for Jacques van Helden
-#MOTIFDB_DIR=/packages/rsat/public_html/motif_databases
-#RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out eeadcsiccompbio/rsat:20240709 rsat
-#MOTIFDB_DIR=~/packages/rsat/motif_databases
+# MOTIFDB_DIR=~/packages/rsat/motif_databases
+# MOTIFDB_DIR=/packages/rsat/public_html/motif_databases
+# RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out eeadcsiccompbio/rsat:20240709 rsat
 
 
 ################################################################
