2ea4be243d3f529c436c0810d9cf3e41432cf852
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-03 14:27:39 +0200

    config file

---
+++ makefiles/00_config.mk
@@ -0,0 +1,15 @@
+################################################################
+## Computer-specific settings
+
+
+## Configuration for IFB server
+RSAT_CMD=rsat
+MOTIFDB_DIR=/shared/projects/rsat_organism/motif_databases
+
+
+## Local configuration for Jacques van Helden's laptop
+# MOTIFDB_DIR=~/packages/rsat/motif_databases
+# MOTIFDB_DIR=/packages/rsat/public_html/motif_databases
+# RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out eeadcsiccompbio/rsat:20240725 rsat
+
+
