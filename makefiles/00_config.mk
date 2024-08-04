d79145538d3f1a1fe19b9c3de4fc374720c584d0
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-04 09:55:57 +0200

    restored default config for IFB cluster

--- makefiles/00_config.mk
+++ makefiles/00_config.mk
@@ -13,11 +13,11 @@ RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16
 
 ## Local configuration for Jacques van Helden's laptop
 # MOTIFDB_DIR=~/packages/rsat/motif_databases
-MOTIFDB_DIR=/packages/rsat/public_html/motif_databases
-RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out eeadcsiccompbio/rsat:20240725 rsat
-SCHEDULER=time
-SLURM_OUT=
-RUNNER=bash
-RUNNER_HEADER="\#!/bin/bash"
+# MOTIFDB_DIR=/packages/rsat/public_html/motif_databases
+# RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out eeadcsiccompbio/rsat:20240725 rsat
+# SCHEDULER=time
+# SLURM_OUT=
+# RUNNER=bash
+# RUNNER_HEADER="\#!/bin/bash"
 
 
