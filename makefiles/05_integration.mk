08fdb6a08e8497735d83bd2355f35c59dad678cf
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-04 09:49:35 +0200

    implemented quality_one_tf target

--- makefiles/05_integration.mk
+++ makefiles/05_integration.mk
@@ -10,13 +10,11 @@ V=2
 targets: targets_00
 	@echo
 	@echo "Clustering motifs across data types"
-	@echo "	cluster_one_tf		cluster motifs discovered across all the data types for a given transcription factor"
-	@echo "	all_metadata		concatenate metadata files of all the data types"
 	@echo "	all_tfs			run a task of each TF"
-	@echo "	cluster_all_tfs		cluster motifs for each TF"
-#	@echo " quality_one_tf            benchmark (matrix-quality) motifs discovered across all the data types for a given transcription factor"
-#	@echo " quality_all_tfs           benchmark (matrix-quality) motifs for each TF"	
-
+	@echo "	cluster_one_tf		cluster motifs discovered across all the data types for a given transcription factor"
+	@echo "	cluster_all_tfs		run cluster_on_tf on each TF"
+	@echo "	quality_one_tf		run matrix-quality on the matrix-clustering result for a given transcription factor"
+	@echo "	quality_all_tfs         run quality_one_tf on each TF"	
 
 
 param:: param_00
@@ -34,20 +32,32 @@ param:: param_00
 	@echo "	TFCLUST_SCRIPT		${TFCLUST_SCRIPT}"
 	@echo "	TFCLUST_ALL_MOTIFS	${TFCLUST_ALL_MOTIFS}"
 	@echo "	TFCLUST_ROOT_MOTIFS	${TFCLUST_ROOT_MOTIFS}"
-	@echo "	TFQUAL_DIR		${TFQUAL_DIR}"
+	@echo
+	@echo "Quality of the clustered motifs"
+	@echo "	MATRIXQ_CMD		${MATRIXQ_CMD}"
+	@echo "	MATRICES		${MATRICES}"
+	@echo "	MATRIXQ_SEQ_OPT		${MATRIXQ_SEQ_OPT}"
+	@echo "	MATRIXQ_SEQ_PLOT_OPT	${MATRIXQ_SEQ_PLOT_OPT}"
+	@echo "	MATRIXQ_SEQ_PERM	${MATRIXQ_SEQ_PERM}"
+	@echo "	MATRIXQ_SEQ_PERM_OPT	${MATRIXQ_SEQ_PERM_OPT}"
+	@echo "	MATRIXQ_DIR		${MATRIXQ_DIR}"
+	@echo "	MATRIXQ_SCRIPT		${MATRIXQ_SCRIPT}"
 	@echo
 
 DATA_TYPE=all-types
 DATASET=all-sets
 
 ################################################################
-## Generate a metadata file with all the datasets for all the TFs
-ALL_METADATA=metadata/${BOARD}/TF_DATASET_all-types.tsv
-all_metadata:
-	ls -1  metadata/${BOARD}/TF_DATASET_* \
-		| grep -v ${ALL_METADATA} \
-		| xargs cat > ${ALL_METADATA}
-	@echo "	ALL_METADATA	${ALL_METADATA}"
+## Iterate a task over each TF
+ALL_TFS=`cat ${ALL_METADATA} | cut -f 1 | sort -u | xargs`
+TF_TASK=cluster_one_tf
+all_tfs:
+	@echo
+	@echo "Running task on all TFs: ${TF_TASK}"
+	@for tf in ${ALL_TFS} ; do \
+		${MAKE} ${TF_TASK} TF=$${tf} ; \
+	done
+
 
 
 TF=LEF1
@@ -57,7 +67,6 @@ TFCLUST_PREFIX=${TFCLUST_DIR}/matrix-clustering
 TFCLUST_ROOT_MOTIFS=${TFCLUST_PREFIX}_cluster_root_motifs
 TFCLUST_ALL_MOTIFS=${TFCLUST_PREFIX}_aligned_logos/All_concatenated_motifs
 TFCLUST_SCRIPT=${TFCLUST_PREFIX}_cmd.sh
-TFQUAL_DIR=${TFCLUST_DIR}/matrix-quality
 #TFCLUST_CMD=find results/${BOARD}/train/*/${TF} -name 'peak-motifs*_motifs_discovered.tf' | awk -F'/' '{print " -matrix "$$4":"$$5":"$$6" "$$0" transfac"}' | xargs ${SCHEDULER} ${RSAT_CMD} matrix-clustering -v ${V} -hclust_method average -calc sum -title ${TF} -metric_build_tree Ncor -lth w 5 -lth cor 0.6 -lth Ncor 0.4 -quick -label_in_tree name -return json,heatmap  -o ${TFCLUST_PREFIX}
 
 ## Define the matrices to use as input for matrix-clustering and matrix-quality
@@ -73,11 +82,34 @@ SLURM_OUT=./slurm_out/TFQUALITY_${BOARD}_cross-data-types-bench_${TF}_slurm-job_
 
 #make -f makefiles/00_parameters.mk matrix_quality DATA_TYPE=CHS TF=GABPA DATASET=THC_0866 MATRICES=results/${BOARD}/train/cross-data-types/GABPA/matrix-clustering_cluster_root_motifs FASTA_SEQ=data/${BOARD}/train/CHS/GABPA/THC_0866.fasta TEST_SEQ=data/${BOARD}/test/CHS_participants.fasta MATRIXQ_DIR=results/${BOARD}/train/cross-data-types/GABPA/matrix-clustering_cluster_root_motifs
 
+
+MATRICES=${TFCLUST_ROOT_MOTIFS}
+MATRIXQ_DIR=${MATRICES}/matrix-quality
+MATRIXQ_SCRIPT=${MATRIXQ_PREFIX}cmd.sh
+MATRIXQ_SEQ_OPT=`awk -F'\t' '$$1=="${TF}" {print "-seq "$$2" data/leaderboard/"$$5"/"$$4"/"$$1"/"$$2".fasta"}' metadata/leaderboard/TF_DATASET_all-types.tsv  | xargs`
+MATRIXQ_SEQ_PLOT_OPT=`awk -F'\t' '$$1=="${TF}" {print "-plot "$$2" nwd"}' metadata/leaderboard/TF_DATASET_all-types.tsv  | xargs`
+MATRIXQ_SEQ_PERM_OPT=`awk -F'\t' '$$1=="${TF}" {print "-perm "$$2" ${MATRIXQ_PERM}"}' metadata/leaderboard/TF_DATASET_all-types.tsv  | xargs`
 quality_one_tf:
 	@echo
-	@echo "Benchmarking motifs across all data types for TF ${TF} with matrix-quality"
+	@echo "Running matrix-quality on matrix-clustering result for all the motifs of TF ${TF}"
+	@echo "	MATRICES		${MATRICES}"
+	@echo "	MATRIXQ_SCRIPT	${MATRIXQ_SCRIPT}"
+	@mkdir -p ${MATRIXQ_DIR}
+	@echo ${RUNNER_HEADER} > ${MATRIXQ_SCRIPT}
+	@echo >> ${MATRIXQ_SCRIPT}
+	@echo ${MATRIXQ_CMD} >> ${MATRIXQ_SCRIPT}
+#	@${RUNNER} ${MATRIXQ_SCRIPT}
+	@echo "	MATRIXQ_DIR	${MATRIXQ_DIR}"
+
+################################################################
+## Run matrix-clustering for each TF, with all the matrices discovered
+## in all the datasets
+quality_all_tfs: all_metadata
 	@echo
-	matrix_quality
+	@echo "Running matrix-quality on the matrix-clustering results for each TF"
+	@${MAKE} all_tfs TF_TASK=quality_one_tf
+
+
 
 
 ################################################################
@@ -89,17 +121,26 @@ cluster_one_tf:
 	@echo
 	@echo "Clustering motifs across all data types for TF ${TF}"
 	@echo
-	@echo "Writing matrix-clustering script	${TFCLUST_SCRIPT}"
+	@echo "Writing matrix-clustering script"
+	@echo "	TFCLUST_SCRIPT	${TFCLUST_SCRIPT}"
 	@mkdir -p ${TFCLUST_DIR}
-	@echo ${SBATCH_HEADER} > ${TFCLUST_SCRIPT}
+	@echo ${RUNNER_HEADER} > ${TFCLUST_SCRIPT}
 	@echo >> ${TFCLUST_SCRIPT}
 	@echo ${TFCLUST_CMD} >> ${TFCLUST_SCRIPT}
 	@echo
 	@echo ${MAKE} ${TFCLUST_ROOT_MOTIFS}_freq.tf ${TFCLUST_ROOT_MOTIFS}_freq.cb ${TFCLUST_ROOT_MOTIFS}_freq.txt >>  ${TFCLUST_SCRIPT}
 	@echo ${MAKE} ${TFCLUST_ALL_MOTIFS}_freq.tf ${TFCLUST_ALL_MOTIFS}_freq.cb ${TFCLUST_ALL_MOTIFS}_freq.txt >>  ${TFCLUST_SCRIPT}
 	@echo
-	@${SBATCH} ${TFCLUST_SCRIPT}
+	@${RUNNER} ${TFCLUST_SCRIPT}
 	@echo "	TFCLUST_DIR	${TFCLUST_DIR}"
+
+################################################################
+## Run matrix-clustering for each TF, with all the matrices discovered
+## in all the datasets
+cluster_all_tfs: all_metadata
+	@echo "Clustering motifs per TF across all data types"
+	@${MAKE} all_tfs TF_TASK=cluster_one_tf
+
 
 
 ################################################################
@@ -120,24 +161,5 @@ tfclust_to_ibis:
 	@echo "	${TFCLUST_ALL_MOTIFS}_freq.cb"
 	@echo "	${TFCLUST_ALL_MOTIFS}_freq.txt"
 
-
-
-################################################################
-## Run matrix-clustering for each TF, with all the matrices discovered
-## in all the datasets
-ALL_TFS=`cat ${ALL_METADATA} | cut -f 1 | sort -u | xargs`
-TF_TASK=cluster_one_tf
-cluster_all_tfs: all_metadata
-	@echo "Clustering motifs per TF across all data types"
-	@${MAKE} all_tfs TF_TASK=cluster_one_tf
-
-################################################################
-## Iterate a task over each TF
-all_tfs:
-	@echo
-	@echo "Running task on all TFs: ${TF_TASK}"
-	@for tf in ${ALL_TFS} ; do \
-		${MAKE} ${TF_TASK} TF=$${tf} ; \
-	done
 
 
