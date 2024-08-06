d2565cb4656e3e81d7ebcb275e652f4216ebb645
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-06 12:11:26 +0200

    new release of RSAT docker container

--- makefiles/00_config.mk
+++ makefiles/00_config.mk
@@ -14,10 +14,12 @@ RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16
 ## Local configuration for Jacques van Helden's laptop
 # MOTIFDB_DIR=~/packages/rsat/motif_databases
 # MOTIFDB_DIR=/packages/rsat/public_html/motif_databases
-# RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out eeadcsiccompbio/rsat:20240725 rsat
-# SCHEDULER=time
-# SLURM_OUT=
-# RUNNER=bash
-# RUNNER_HEADER="\#!/bin/bash"
+#DOCKER_RELEASE=eeadcsiccompbio/rsat:20240806
+#RSAT_CMD=rsat
+#RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out ${DOCKER_RELEASE} rsat
+#SCHEDULER=time
+#SLURM_OUT=
+#RUNNER=bash
+#RUNNER_HEADER="\#!/bin/bash"
 
 
