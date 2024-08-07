0f8242c314729e4879d35378e556a2c701279a36
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-07 10:30:50 +0200

    implemented target scan_one_dataset

--- makefiles/06_score_sequences.mk
+++ makefiles/06_score_sequences.mk
@@ -6,7 +6,7 @@
 
 
 #include makefiles/00_parameters.mk 
-include makefiles/05_integration.mk
+include makefiles/00_parameters.mk
 MAKEFILE=makefiles/06_score_sequences.mk
 
 targets: targets_00
@@ -21,15 +21,39 @@ targets: targets_00
 param:: param_00
 	@echo
 	@echo "Sequence scoring parameters"
-	@echo "	RAND_SEQ		${RAND_SEQ}"
-	@echo "	RAND_SCRIPT		${RAND_SCRIPT}"
-	@echo "	SCAN_DIR		${SCAN_DIR}"
+	@echo "==========================="
+	@echo
+	@echo "Random sequences"
+	@echo "	RAND_SEQ	${RAND_SEQ}"
+	@echo "	RAND_SCRIPT	${RAND_SCRIPT}"
+	@echo
+	@echo "Matrices to evaluate"
+	@echo "	MATRICES	${MATRICES}"
+	@echo
+	@echo "Scanning sequences"
+	@echo "	SCAN_DIR	${SCAN_DIR}"
+	@echo "	MATRICES	${MATRICES}"
+	@echo "	FASTA_SEQ	${FASTA_SEQ}"
+	@echo "	SCAN_SCRIPT	${SCAN_SCRIPT}"
+	@echo "	SCAN_CMD	${SCAN_CMD}"
+	@echo "	SCAN_RESULT	${SCAN_RESULT}"
+
+
+################################################################
+## Define the matrices to use as input for matrix-clustering and
+## matrix-quality. We initially restricted the analysis to the root
+## motifs but these motifs are too degenerated -> we apply it to all
+## the nodes of the matrix clustering result tree.
+DATA_TYPE=CSH
+TF=GABPA
+DATASET=THC_0866
+MATRICES=${TFCLUST_ALL_MOTIFS}_trimmed
 
 ################################################################
 ## Select random genomic sequences of the same lengths as the current
 ## data set
 RAND_SEQ=${DATASET_PATH}_random-genome-fragments.fa
-RAND_CMD=${RSAT_CMD} random-genome-fragments  \
+RAND_CMD=${SCHEDULER} ${RSAT_CMD} random-genome-fragments  \
 		-template_format fasta \
 		-i ${FASTA_SEQ} \
 		-org Homo_sapiens_GCF_000001405.40_GRCh38.p14  \
@@ -58,10 +82,13 @@ rand_fragments_all_datatypes:
 ################################################################
 ## Scan one sequence set with a given matrix file
 ##
+DATA_TYPE=CHS
+DATASET=
 SCAN_DIR=results/${BOARD}/train/${DATA_TYPE}/${TF}/scan
+SCAN_SCRIPT=${SCAN_DIR}/scanning_cmd.sh
 SCAN_RESULT=${SCAN_DIR}/${TF}_${DATASET}_scan_top-per-seq.tsv
-SCAN_CMD=${RSAT_CMD} matrix-scan -v ${V} \
-	-m ${MATRICES} \
+SCAN_CMD=${SCHEDULER} ${RSAT_CMD} matrix-scan -v ${V} \
+	-m ${MATRICES}.tf \
 	-matrix_format transfac \
 	-i ${FASTA_SEQ} \
 	-seq_format fasta \
@@ -79,7 +106,19 @@ SCAN_CMD=${RSAT_CMD} matrix-scan -v ${V} \
 scan_one_dataset:
 	@echo "Scanning sequences"
 	@echo "	SCAN_DIR		${SCAN_DIR}"
+	@echo "	BOARD			${BOARD}"
+	@echo "	DATA_TYPE		${DATA_TYPE}"
+	@echo "	TF			${TF}"
+	@echo "	DATASET			${DATASET}"
 	@echo "	MATRICES		${MATRICES}"
 	@echo "	FASTA_SEQ		${FASTA_SEQ}"
 	@echo "	SCAN_CMD		${SCAN_CMD}"
-	${RUNNER} ${SCAN_CMD}
+	@echo "	Writing matrix-clustering script"
+	@echo "	SCAN_SCRIPT	${SCAN_SCRIPT}"
+	@mkdir -p ${SCAN_DIR}
+	@echo ${RUNNER_HEADER} > ${SCAN_SCRIPT}
+	@echo >> ${SCAN_SCRIPT}
+	@echo ${SCAN_CMD} >> ${SCAN_SCRIPT}
+	@${RUNNER} ${SCAN_SCRIPT}
+	@echo "	SCAN_RESULT		${SCAN_RESULT}"
+	@echo
