c5489eec07fe40a505b21a3dff41ffb55ba0d488
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-08-08 19:27:56 +0200

    added make config file for local apptainer container on IFB-core-cluster

---
+++ makefiles/running_config_apptainer.mk
@@ -0,0 +1,18 @@
+################################################################
+## Computer-specific settings
+
+
+## Configuration for IFB core cluster (core.cluster.france-bioinformatique.fr)
+MOTIFDB_DIR=/shared/projects/rsat_organism/motif_databases
+SCHEDULER=srun time
+SLURM_OUT=./slurm_out/${BOARD}_${DATA_TYPE}_${TF}_${DATASET}_slurm-job_%j.out
+RUNNER=sbatch
+RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16G\n"
+
+
+## Local configuration for Apptainer on IFB core cluster
+DOCKER_RELEASE=20240808
+DOCKER_IMAGE=eeadcsiccompbio/rsat:${DOCKER_RELEASE}
+#RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out ${DOCKER_IMAGE} rsat
+RSAT_CMD=rsat_apptainer/rsat_${DOCKER_RELEASE}.sif rsat
+
