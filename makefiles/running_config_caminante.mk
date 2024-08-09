52e0d486d6cd85a9afafc03e6f081b8343e66bdf
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-09 06:59:09 +0200

    running config for caminante uses new docker build 20240808

--- makefiles/running_config_caminante.mk
+++ makefiles/running_config_caminante.mk
@@ -3,16 +3,14 @@
 ##
 ## Local configuration for Jacques van Helden's laptop
 
-
-
-
+## Use RSAT package installed on the laptop
 RSAT_CMD=rsat
 MOTIFDB_DIR=~/packages/rsat/motif_databases ## Local RSAT installation
 
-## Local docker container
-# DOCKER_RELEASE=eeadcsiccompbio/rsat:20240806
-# RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out ${DOCKER_RELEASE} rsat
-# MOTIFDB_DIR=/packages/rsat/public_html/motif_databases # in the Docker container
+## Use docker container
+DOCKER_RELEASE=eeadcsiccompbio/rsat:20240808
+RSAT_CMD=docker run -v $$PWD:/home/rsat_user -v $$PWD/results:/home/rsat_user/out ${DOCKER_RELEASE} rsat
+MOTIFDB_DIR=/packages/rsat/public_html/motif_databases # in the Docker container
 
 SCHEDULER=time
 RUNNER=bash
