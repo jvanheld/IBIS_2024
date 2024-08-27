a982b1fec18c96ba05ed284314eeb1c08f85b965
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-08-27 07:43:21 +0200

    updated configuration and log directory

--- makefiles/running_config_ifb-core-cluster.mk
+++ makefiles/running_config_ifb-core-cluster.mk
@@ -5,9 +5,8 @@
 ## Configuration for IFB core cluster (core.cluster.france-bioinformatique.fr)
 RSAT_CMD=rsat
 MOTIFDB_DIR=/shared/projects/rsat_organism/motif_databases
+SLURM_OUT=${LOG_DIR}/${BOARD}_${EXPERIMENT}_${TF}_${DATASET}_slurm-job_%j.out
 SCHEDULER=srun time
-TODAY=date '+%Y-%m-%d'
-SLURM_OUT=./slurm_out/${TODAY}/${BOARD}_${EXPERIMENT}_${TF}_${DATASET}_slurm-job_%j.out
 RUNNER=sbatch
 RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16G\n"
 
