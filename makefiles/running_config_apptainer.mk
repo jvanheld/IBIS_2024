7b15f4a6cbf5a84fba4bc2aacc516e57dbec2224
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-08-27 07:43:17 +0200

    updated configuration and log directory

--- makefiles/running_config_apptainer.mk
+++ makefiles/running_config_apptainer.mk
@@ -6,11 +6,11 @@
 ################################################################
 ## Configuration for IFB core cluster (core.cluster.france-bioinformatique.fr)
 MOTIFDB_DIR=/shared/projects/rsat_organism/motif_databases
+SLURM_OUT=${LOG_DIR}/${BOARD}_${EXPERIMENT}_${TF}_${DATASET}_slurm-job_%j.out
 SCHEDULER=srun time
-TODAY=date '+%Y-%m-%d'
-SLURM_OUT=./slurm_out/${BOARD}_${EXPERIMENT}_${TF}_${DATASET}_slurm-job_%j.out
 RUNNER=sbatch
-RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16G\n"
+#RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16G\n\n"
+RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem=32G\n\n"
 
 
 ################################################################
