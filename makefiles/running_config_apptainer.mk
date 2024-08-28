48f98a67a025310987b5bf5ff7e9166c1552afc5
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-08-28 14:44:01 +0200

    --mem=16GB instead of 32GB

--- makefiles/running_config_apptainer.mk
+++ makefiles/running_config_apptainer.mk
@@ -10,7 +10,7 @@ SLURM_OUT=${LOG_DIR}/${BOARD}_${EXPERIMENT}_${TF}_${DATASET}_slurm-job_%j.out
 SCHEDULER=srun time
 RUNNER=sbatch
 #RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16G\n\n"
-RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem=32G\n\n"
+RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem=16G\n\n"
 
 
 ################################################################
