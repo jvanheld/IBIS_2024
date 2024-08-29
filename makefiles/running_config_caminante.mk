fb7f8080460ddef8021517c32f10380a3ceb3ace
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-29 05:07:27 +0200

    added targets usage, docker_pull and run

--- makefiles/running_config_caminante.mk
+++ makefiles/running_config_caminante.mk
@@ -41,4 +41,27 @@ OMGA_PATH=${OMGA_DIR}/optimize-matrix-GA.py
 OMGA_PYTHON_PATH=${OMGA_DIR}/venv/Downloads/bin/python
 OMGA_CMD_PREFIX=${OMGA_PYTHON_PATH} ${OMGA_PATH}
 
+usage:
+	@echo
+	@echo "Configuration for JvH laptop"
+	@echo
+	@echo "Parameters"
+	@echo "	DOCKER_RELEASE		${DOCKER_RELEASE}"
+	@echo "	RSAT_MODE		${RSAT_MODE}"
+	@echo "	RSAT_CMD		${RSAT_CMD}"
+	@echo "	MOTIFDB_DIR		${MOTIF_DB_DIR}"
+	@echo "	OMGA_DIR		${OMGA_DIR}"
+	@echo "	OMGA_CMD_PREFIX		${OMGA_CMD_PREFIX}"
+	@echo "	THREADS			${THREADS}"
+	@echo
+	@echo "Targets"
+	@echo "	usage			print usage for the current makefile"
+	@echo "	docker_pull		download and install the docker image on this computer"
+	@echo "	run			run rsat command"
+
+docker_pull:
+	docker pull eeadcsiccompbio/rsat:${DOCKER_RELEASE}
+
+run:
+	${RSAT_CMD}
 
