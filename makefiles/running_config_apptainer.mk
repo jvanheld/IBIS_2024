f62043b16450fb2c206d230ee36991c4ed0825e0
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-08-30 14:30:03 +0200

    added local temporary dir to sbatch header

--- makefiles/running_config_apptainer.mk
+++ makefiles/running_config_apptainer.mk
@@ -10,7 +10,7 @@ SLURM_OUT=${LOG_DIR}/${BOARD}_${EXPERIMENT}_${TF}_${DATASET}_slurm-job_%j.out
 SCHEDULER=srun time
 RUNNER=sbatch
 #RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16G\n\n"
-RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem=16G\n\n"
+RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem=16G\nTMPDIR=/shared/projects/ibis_challenge/tmp/\nTMP=\$${TMPDIR}\nTEMP=\$${TMPDIR}\nmkdir -p \$${TMPDIR}\nexport TMPDIR TMP TEMP\n\n"
 
 
 ################################################################
