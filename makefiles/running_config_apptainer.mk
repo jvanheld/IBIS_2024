2438af68c131f5eb90ba9e814ace1d3074cee115
jvanheld <Jacques.van-Helden@univ-amu.fr>
2024-08-30 16:26:26 +0200

    fixed problem with RUNNER_HEADER

--- makefiles/running_config_apptainer.mk
+++ makefiles/running_config_apptainer.mk
@@ -9,9 +9,7 @@ MOTIFDB_DIR=/shared/projects/rsat_organism/motif_databases
 SLURM_OUT=${LOG_DIR}/${BOARD}_${EXPERIMENT}_${TF}_${DATASET}_slurm-job_%j.out
 SCHEDULER=srun time
 RUNNER=sbatch
-#RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem-per-cpu=16G\n\n"
-RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem=16G\nTMPDIR=/shared/projects/ibis_challenge/tmp/\nTMP=\$${TMPDIR}\nTEMP=\$${TMPDIR}\nmkdir -p \$${TMPDIR}\nexport TMPDIR TMP TEMP\n\n"
-
+RUNNER_HEADER="\#!/bin/bash\n\#SBATCH -o ${SLURM_OUT}\n\#SBATCH --mem=16G\n\nTMPDIR=/shared/projects/ibis_challenge/tmp/\nTMP=\$${TMPDIR}\nTEMP=\$${TMPDIR}\nmkdir -p \$${TMPDIR}\nexport TMPDIR TMP TEMP\n\n"
 
 ################################################################
 ## Local configuration for Apptainer on IFB core cluster
